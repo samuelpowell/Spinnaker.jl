ee2af64d01e1d2d1bac99ac72ad18fba8c7437a0
Ian Butterworth <ibutter@mit.edu>
2018-11-29 20:16:16 -0500

    removing erroneous addition

--- src/wrapper/spin_api.jl
+++ src/wrapper/spin_api.jl
@@ -293,10 +293,6 @@ function spinCameraDeInit(hCamera)
     checkerror(ccall((:spinCameraDeInit, libSpinnaker_C), spinError, (spinCamera,), hCamera))
 end
 
-function spinCameraGetID(hCamera, id)
-    checkerror(ccall((:spinCameraGetID, libSpinnaker_C), spinError, (spinCamera, Ptr{Cvoid}), hCamera, id))
-end
-
 function spinCameraGetNodeMap(hCamera, phNodeMap)
     checkerror(ccall((:spinCameraGetNodeMap, libSpinnaker_C), spinError, (spinCamera, Ptr{spinNodeMapHandle}), hCamera, phNodeMap))
 end
