120edb165464dcf89426778936c307fe59f11660
Ian <i.r.butterworth@gmail.com>
2019-10-03 09:10:04 -0400

    gracefully disable CI tests, and error early if no camera detected otherwise

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,37 +1,57 @@
-using Spinnaker, Test, ImageCore
+using Test, ImageCore
+function is_ci()
+    get(ENV, "TRAVIS", "") == "true" ||
+    get(ENV, "APPVEYOR", "") in ("true", "True") ||
+    get(ENV, "CI", "") in ("true", "True")
+end
 
-@testset "Camera Interaction" begin
+if is_ci()
+    @info "CI testing is disabled due to the need for a Spinnaker-compatible camera during tests."
+else
+    using Spinnaker
+
     camlist = CameraList()
-    @test length(camlist) >= 1
-    cam = camlist[0]
-    @test typeof(cam) == Spinnaker.Camera
+    if length(camlist) < 1
+        error("""Spinnaker could not find a compatible camera.
+        Tests require that a Spinnaker-compatible camera is connected and discoverable.
+        If such a camera is connected, check that it is discoverable in SpinView""")
+    else
+        @testset "Camera Interaction" begin
+            cam = camlist[0]
+            @test typeof(cam) == Spinnaker.Camera
 
-    @testset "Set continuous mode" begin
-        triggermode!(cam, "Off")
-        @test triggermode(cam) == "Off"
+            @testset "Set continuous mode" begin
+                triggermode!(cam, "Off")
+                @test triggermode(cam) == "Off"
 
-        acquisitionmode!(cam, "Continuous")
-        @test acquisitionmode(cam) == "Continuous"
+                acquisitionmode!(cam, "Continuous")
+                @test acquisitionmode(cam) == "Continuous"
 
-        framerate!(cam, 60)
-        @test framerate(cam) == 60
-    end
+                framerate!(cam, 60.0)
+                @test isapprox(framerate(cam), 60.0)
+            end
 
-    @testset "Set exposure" begin
-        exposure!(cam, 4000)
-        exp, mode = exposure(cam)
-        @test isapprox(exp, 4000, rtol=0.1)
-        @test mode == "Off"
+            @testset "Set exposure" begin
+                exposure!(cam, 4000)
+                exp, mode = exposure(cam)
+                @test isapprox(exp, 4000, rtol=0.1)
+                @test mode == "Off"
 
-        exposure!(cam)
-        exp, mode = exposure(cam)
-        @test mode == "Continuous"
-    end
+                exposure!(cam)
+                exp, mode = exposure(cam)
+                @test mode == "Continuous"
+            end
 
-    @testset "Read image" begin
-        start!(cam)
-        img = getimage(cam, Gray{N0f8}, normalize=true)
-        @test all(size(img) .> 0)
-        stop!(cam)
+            @testset "Read image" begin
+                @testset "Mono8" begin
+                    pixelformat!(cam, "Mono8")
+                    @assert pixelformat(cam) == "Mono8"
+                    start!(cam)
+                    img = getimage(cam, Gray{N0f8}, normalize=true)
+                    @test all(size(img) .> 0)
+                    stop!(cam)
+                end
+            end
+        end
     end
 end
