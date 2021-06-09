98d679f4a1b197ed1bd9eb2d76cdabb48a91ec8e
Ian <i.r.butterworth@gmail.com>
2021-06-09 18:07:31 -0400

    add basic digitalio funcs

---
+++ src/camera/digitalio.jl
@@ -0,0 +1,28 @@
+# Spinnaker.jl: wrapper for FLIR/Point Grey Spinnaker SDK
+# Copyright (C) 2019 Samuel Powell
+
+# camera/digitalio.jl: Camera object digital io features
+
+module DigitalIO
+
+"""
+  line_mode(::Camera, state::Symbol)
+  Sets the DigitalIO Line Mode. State can either be `:input` or `:output`.
+"""
+function line_mode(cam::Camera, state::Symbol)
+  @assert state in [:input, :output] "State can only be either :input or :output"
+  if state == :input
+    set!(SpinEnumNode(cam, "LineMode"), LineMode_Input)
+  else if state == :output
+    set!(SpinEnumNode(cam, "LineMode"), LineMode_Output)
+  end
+  return nothing
+end
+
+"""
+  v3_3_enable(::Camera, state::Bool)
+  Sets the DigitalIO 3.3V line state.
+"""
+v3_3_enable(cam::Camera, state::Bool) = set!(SpinBooleanNode(cam, "V3_3Enable"), state)
+
+end #module
